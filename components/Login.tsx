58fd6afeb74a29b86258f24ae0db709a945bf551
batihanozio <vexedeity@DESKTOP-6VJ2J89>
2023-02-11 02:24:29 +0300

    debug update

--- components/Login.tsx
+++ components/Login.tsx
@@ -4,13 +4,15 @@ import Image from "next/image";
 import { chatgptlogo } from "../assets/img";
 
 type Props = {};
-
+function trySignIn() {
+  signIn("google").catch((e) => console.log(e));
+}
 const Login = ({}: Props) => {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-[#11A37F] text-center">
       <Image src={chatgptlogo} alt="Logo" width={300} height={300} />
       <button
-        onClick={() => signIn("google")}
+        onClick={trySignIn}
         className="animate-pulse text-3xl font-bold text-white"
       >
         Sign In to use ChatGPT
