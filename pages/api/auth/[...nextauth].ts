57600057e7726cf52df7c6bb218c5b9b1320cf32
batihanozio <vexedeity@DESKTOP-6VJ2J89>
2023-02-11 00:04:08 +0300

    trying to fix google auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth";
+import NextAuth, { User as NextAuthUser } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+interface NextAuthUserWithStringId extends NextAuthUser {
+  id: string;
+}
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,6 +10,14 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        } as NextAuthUserWithStringId;
+      },
     }),
     // ...add more providers here
   ],
