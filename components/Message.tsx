fa8f4464dc029ea1de5c9be3ea003aa0b6ac836e
batihanozio <batihandev@gmail.com>
2023-04-09 18:03:37 +0300

    image fix

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { DocumentData } from "firebase/firestore";
+import Image from "next/image";
 import { chatgptAvatar } from "../assets/img";
 type Props = {
   message: DocumentData;
@@ -9,15 +10,18 @@ function Message({ message }: Props) {
   return (
     <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
       <div className="mx-auto flex max-w-2xl space-x-5 px-10">
-        <img
-          src={
-            message.user.avatar === "chatGPT"
-              ? chatgptAvatar.src
-              : message.user.avatar
-          }
-          alt=""
-          className="h-8 w-8"
-        />
+        {isChatGPT ? (
+          <Image
+            alt="ChatGPT"
+            src={chatgptAvatar}
+            width={32}
+            height={32}
+            className="h-8 w-8"
+          />
+        ) : (
+          <img src={message.user.avatar} alt="" className="h-8 w-8" />
+        )}
+
         <p className="pt-1 text-sm">{message.text}</p>
       </div>
     </div>
